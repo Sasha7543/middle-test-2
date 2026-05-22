309ffda18ca90691f0c3f2417a31217688ea95a3
Sasha7543 <lisijsasa70@gmail.com>
2026-05-22 15:37:20 +0300

    Add main recipe view

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from recipe import views
 
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('admin/', admin.site.urls),
 ]
