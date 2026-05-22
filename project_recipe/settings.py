5768cc16a5f56d24b7a5555d8be70e49fd46d3f0
Sasha7543 <lisijsasa70@gmail.com>
2026-05-22 15:46:00 +0300

    Fix GitHub Actions test workflow

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -24,7 +24,7 @@ load_env_file(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-development-key')
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'django-insecure-local-development-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
